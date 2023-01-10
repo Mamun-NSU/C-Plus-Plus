83010c6b02167ffd965543eaf0b8438fa9186f4f
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2023-01-10 22:33:59 +0600

    Array Queries solved successfully

---
+++ Array Queries.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number;
+    cin >> number;
+
+    while (number--)
+    {
+
+        int arrNum, testCase;
+        cin >> arrNum >> testCase;
+
+        int arr[arrNum];
+
+        for (int i = 0; i < arrNum; i++)
+        {
+            cin >> arr[i];
+        }
+
+        cout << "Case " << number++ << ":\n";
+
+        while (testCase--)
+        {
+
+            int firstIndx, lastIndx;
+            cin >> firstIndx >> lastIndx;
+
+            int minValue = arr[firstIndx - 1];
+
+            for (int i = firstIndx - 1; i < lastIndx; i++)
+            {
+                if (arr[i] < minValue)
+                {
+                    minValue = arr[i];
+                }
+            }
+            cout << minValue;
+            cout << "\n";
+        }
+    }
+
+    return 0;
+}
