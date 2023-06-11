662618930bcacef4a50e68f8d44a7d7944f66e63
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2023-06-11 23:18:26 +0600

    get prefix sum solved successfully

---
+++ getPrefixSum.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    // Write your code here
+    int N;
+    cin >> N;
+    int A[N];
+
+    long long B[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+
+        if (i == 0)
+        {
+            B[i] = A[i];
+        }
+        else
+        {
+            B[i] = A[i] + B[i - 1];
+        }
+    }
+
+    for (int i = N - 1; i >= 0; i--)
+    {
+        cout << B[i] << " ";
+    }
+    return 0;
+}
